88aa50ea9156e8a6facdfac887916570123af439
itay <itay.shviro@gmail.com>
2026-05-18 21:40:17 +0000

    finish crd

--- api/v1alpha1/namespacelabel_types.go
+++ api/v1alpha1/namespacelabel_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -25,37 +9,18 @@ import (
 
 // NamespaceLabelSpec defines the desired state of NamespaceLabel
 type NamespaceLabelSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of NamespaceLabel. Edit namespacelabel_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+// Labels is the map of key-value pairs that should be applied to the Namespace
+// +kubebuilder:validation:Required
+	Labels map[string]string `json:"labels"`
 }
 
 // NamespaceLabelStatus defines the observed state of NamespaceLabel.
 type NamespaceLabelStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Applied indicates if the labels were successfully synced to the Namespace
+	Applied bool   `json:"applied"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the NamespaceLabel resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
+	// Message provides details about the sync status, such as failure reasons
+	Message string `json:"message,omitempty"`
 }
 
 // +kubebuilder:object:root=true
